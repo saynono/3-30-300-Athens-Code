14bf4ad5ed61eaea4a900f47505afe600993e298
nono <vincent@say-nono.com>
2024-11-05 12:02:23 +0200

    Something... not sure what.

---
+++ Treepedia/Action-300-Tests.py
@@ -0,0 +1,83 @@
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import matplotlib.pyplot as plt
+
+import geopandas as gpd
+import osmnx as ox
+# import osmnx.utils_geo
+from pyproj import CRS
+import matplotlib.pyplot as plt
+import networkx as nx
+from shapely.geometry import Point, LineString, Polygon
+from shapely import wkt
+import utils
+import random
+import math
+
+
+
+if __name__ == "__main__":
+    import os, os.path
+
+
+    iteration_no = 3
+
+    # move this to an external specs file
+    # in hectares
+    area_min_size = 1.0
+
+    # in meters
+    max_distance = 300
+
+    root = '/Users/nono/Documents/workspaces/GIS/3-30-300-Athens-data/maps/Kypseli-All/'
+    shape_file = "Kypseli-All.shp"
+
+    root_generated = os.path.join(root, "generated/")
+    shape_file_name = os.path.splitext(os.path.basename(shape_file))[0]
+    shape_file_boundry = shape_file_name+"-Parks-Boundry-temp.shp"
+    shape_file_park_forests = shape_file_name+"-Parks-Forests-All.gpkg"
+    shape_file_park_forests_selected = shape_file_name+"-Parks-Forests-Selected.gpkg"
+    shape_file_routes = shape_file_name+"-Routes.gpkg"
+    shape_file_residential_buildings = shape_file_name+"-Residential-Buildings.gpkg"
+    routes_list = shape_file_name+"-Routes.txt"
+    graph_file = shape_file_name+"-Graph-Walking.graphml"
+
+    inputShp = os.path.join(root, shape_file)
+    outputGraphWalking = os.path.join(root_generated, graph_file)
+
+    gdf_in = gpd.read_file(inputShp)
+
+    if not os.path.exists(outputGraphWalking):
+        print("Couldn't find graph file")
+        exit()
+
+        # graph = nx.read_gml(outputGraphWalking)
+    graph = ox.load_graphml(outputGraphWalking)
+    # graph = ox.save_graph_geopackage(outputGraphWalking)
+    for u, v, key, data in graph.edges(data=True, keys=True):
+        geom = data.get('geometry', None)  # Get the geometry object (e.g., LineString)
+        if isinstance(geom, str):
+            data['geometry'] = wkt.loads(geom)  # Convert the geometry to WKT (string format)
+            # print('Convert please:', geom, data['geometry'])
+
+    parks_and_forests_filtered = utils.get_parks_and_forests(gdf_in, area_min_size, max_distance)
+    # parks_and_forests_filtered.to_file(outputParksForestsSelectedShp, driver="GPKG")
+    parks_and_forests_filtered['park_nodes'] = parks_and_forests_filtered.apply(lambda row: [ox.nearest_nodes(graph, point[0], point[1]) for point in row.geometry.exterior.coords], axis=1)
+
+    selected_park = parks_and_forests_filtered.sample(n=1)
+
+    gdf_entries_near_park = utils.get_entry_points_to_park(graph, selected_park)
+
+
+    # Plot the park boundary
+    ax = selected_park.plot(color="green", edgecolor="black", figsize=(10, 10), alpha=0.5)
+
+    # Plot the entry points on the same plot
+    gdf_entries_near_park.plot(ax=ax, color="red", markersize=20, label="Entry Points")
+
+    plt.legend()
+    plt.title(f"Entry Points for park")
+    plt.show()
+
---
+++ Treepedia/utils.py
@@ -0,0 +1,266 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+with warnings.catch_warnings():
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import requests
+from OSMPythonTools.nominatim import Nominatim
+from shapely.geometry import Point, LineString, Polygon
+import osmnx as ox
+# import osmnx.utils_graph
+import osmnx.routing
+import networkx as nx
+from pyproj import CRS
+import geopandas as gpd
+
+
+
+def get_keys(key_file):
+    lines = open(key_file,"r")
+    keylist = []
+    for line in lines:
+        key = line[:-1]
+        keylist.append(key)
+
+    print('The key list is:=============', keylist)
+
+    return keylist
+
+
+def retrieveAddressFromLocationViaNomination(latitude, longitude):
+    nominatim = Nominatim()
+    # Perform reverse geocoding based on latitude and longitude
+    location = nominatim.query('latitude=37.9836, longitude=23.7275')
+
+    # Print the full address
+    print("::::", type(location), location.toJSON())
+    # Step 2: Use Nominatim to reverse geocode the coordinates
+    # nominatim_url = f"https://nominatim.openstreetmap.org/details.php?format=json&lat={latitude}&lon={longitude}&zoom=18&addressdetails=1"
+    # nominatim_url = f"https://nominatim.openstreetmap.org/reverse.php?lat={latitude}&lon={longitude}&zoom=18&format=jsonv2"
+    #
+    # # Make a request to the Nominatim API
+    # # response = requests.get(nominatim_url)
+    # response = requests.get(nominatim_url)
+    # response.raise_for_status()
+    # print(response.json())
+    #
+    # # Parse the JSON response
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     if 'address' in data:
+    #         address = data['address']
+    #         print(f"Address: {address}")
+    #         return address
+    #     else:
+    #         print("No address found.")
+    # else:
+    #     print("Error contacting Nominatim.")
+    #
+    # return None
+
+    # Step 2: Your Google Maps API key
+    api_key = "AIzaSyDqAHRrEPkCKZdGX0owZtbzCdATlgqbkmE"  # Replace with your actual API key
+
+    # Step 3: Build the request URL
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={api_key}"
+
+    # Step 4: Make the request to Google Geocoding API
+    response = requests.get(url)
+
+    # Step 5: Parse the response JSON
+    if response.status_code == 200:
+        data = response.json()
+        if data['status'] == 'OK':
+            # Get the first result (most relevant address)
+            address = data['results'][0]['formatted_address']
+            print(f"Address: {address}")
+            print(f"Address Raw: {data}")
+        else:
+            print(f"Error: {data['status']}")
+    else:
+        print("Error contacting the Google API.")
+
+
+def get_route_building_park(graph, building, park):
+
+    # if building.geometry and building.geometry.centroid:
+    # if building.geometry is not None:
+    home_location = [building.geometry.centroid.x, building.geometry.centroid.y]
+    # else:
+    #     home_location = [float(building.centroid.x), float(building.centroid.y)]
+
+    # print(building.centroid)
+    # print(type(building.centroid))
+
+    # print(home)
+    # home_location = [23.735704, 37.999781]
+
+    #park
+    # park_location = [23.737078, 37.993286]
+    # selected_park = parks_and_forests_filtered.sample(n=1)
+    # selected_park = parks_and_forests_filtered.iloc[0]
+    # print('selected_park\n', selected_park,selected_park.geometry.centroid)
+    park_boundries = park.geometry
+    park_location = [park.geometry.centroid.x, park.geometry.centroid.y]
+
+
+    # print(f"HOME LOCATION = {home_location}")
+    # print(f"PARK LOCATION = {park_location}")
+
+
+    home_node = ox.nearest_nodes(graph, home_location[0], home_location[1])
+    home_edge = ox.nearest_edges(graph, home_location[0], home_location[1])
+
+    park_node = ox.nearest_nodes(graph, park_location[0], park_location[1])
+    route = nx.shortest_path(graph, home_node, park_node, weight='length')
+    # route = ox.routing.shortest_path(graph, home_node, park_node, weight='length')
+
+    return route
+
+
+def clip_route_to_park_boundries(graph, route, park):
+    route_truncated = []
+    for node in route:
+        point = Point(graph.nodes[node]['x'], graph.nodes[node]['y'])
+        route_truncated.append(node)
+        if park.geometry.contains(point):  # Check if the node is inside the park
+            break  # Stop the route here when the node is inside the park
+    return route_truncated
+
+
+def get_route_length (graph, route):
+    # TODO: Add time to walk:
+    # route_time = int(sum(ox.utils_graph.route_to_gdf(graph, route, "travel_time")["travel_time"]))
+
+    # total_length = sum(ox.utils_graph.get_route_edge_attributes(graph, route, 'length'))
+    total_length = int(sum(ox.routing.route_to_gdf(graph, route, weight="length")["length"]))
+    # ox.distance.
+    # ox.routing.route_to_gdf()
+    # total_length = sum(ox.utils_graph.route_to_gdf(graph, route, 'length'))
+    # route_gdf = ox.routing.route_to_gdf(graph, route, "length", attributes=True)
+    # route2_length = int(sum(ox.utils_graph.route_to_gdf(G, route2, "length")["length"]))
+    # total_length = route_gdf['length'].sum()
+    return total_length
+
+def get_parks_and_forests (shape_orginial, area_min_size, max_distance):
+    # expand the shape file by the defined distance
+    parks_boundry = expand_area(shape_orginial, max_distance)
+
+
+    # Check for invalid geometries
+    invalid_geometries = parks_boundry[~parks_boundry.is_valid]
+    if not invalid_geometries.empty:
+        print("Warning: The GeoDataFrame contains invalid geometries.")
+    if parks_boundry.crs is None:
+        parks_boundry.set_crs(epsg=4326, inplace=True)
+    else:
+        print(f"CRS is set to: {parks_boundry.crs}")
+
+    # Save the new shapefile
+    #     gdf_parks_boundry.to_file(outputShp)
+
+    # Extract the boundary polygon from the GeoDataFrame
+    boundary_polygon = parks_boundry.geometry.unary_union
+
+
+    # Define the OSM tags to search for parks and forests
+    tags = {'leisure': 'park', 'landuse': 'forest'}
+
+    # Download parks and forests from OSM within the boundary polygon
+    parks_and_forests = ox.features_from_polygon(boundary_polygon, tags)
+    parks_and_forests = parks_and_forests.to_crs(epsg=3857)
+    print(f"CRS parks_and_forests is set to: {parks_and_forests.crs}")
+
+    # Step 3: Calculate the area in square meters
+    parks_and_forests['area'] = parks_and_forests.geometry.area
+
+    # Optional: Convert to square kilometers and hectares
+    parks_and_forests['area_km2'] = parks_and_forests['area'] / 1_000_000
+    parks_and_forests['area_ha'] = (parks_and_forests['area'] / 10_000)
+
+
+    # # Step 4: Print the name and area for each shape (assuming 'Name' is the name column)
+    # for index, row in parks_and_forests.iterrows():
+    #     shape_name = row['name']  # Replace 'Name' with the actual column name for shape names
+    #     # shape_name_en = row['name_en']
+    #     shape_area = row['area_hectares']
+    #     print(f"Shape: {shape_name}, Area: {shape_area} hectares")
+
+
+
+    # Save the retrieved parks and forests to a new shapefile
+    # parks_and_forests.to_file(outputParksForestsShp)
+
+    # Filter the GeoDataFrame based on the area
+    parks_and_forests_filtered = parks_and_forests[parks_and_forests['area_ha'] > area_min_size]
+    return parks_and_forests_filtered
+
+
+def expand_area(shapefile_in, expand_by):
+    # Load the shapefile
+
+    shapefile_out = shapefile_in
+    # Reproject to EPSG:3857 if necessary (to ensure distance is in meters)
+    if shapefile_out.crs.to_string() != "EPSG:3857":
+        shapefile_out = shapefile_out.to_crs(epsg=3857)
+
+    # Expand the geometries by 300 meters
+    shapefile_out.geometry = shapefile_out.geometry.buffer(expand_by)
+    # Ensure CRS is still valid after buffering
+    shapefile_out.set_crs(epsg=3857, inplace=True)
+
+    # Apply the function to get the UTM CRS
+    # utm_crs = get_utm_crs(gdf_parks_boundry)
+    utm_crs = CRS.from_string('epsg:4326')
+    # Reproject the GeoDataFrame to the appropriate UTM CRS
+    shapefile_out = shapefile_out.to_crs(utm_crs)
+
+
+    # Check for invalid geometries
+    shapefile_out['valid'] = shapefile_out.is_valid
+    print(f"Number of invalid geometries: {len(shapefile_out[shapefile_out['valid'] == False])}")
+
+    return shapefile_out
+
+
+
+def get_entry_points_to_park (graph, gdf_park):
+    # Define tags for paths, gates, and entrances
+    tags = {
+        "highway": ["footway", "path"],  # Footpaths
+        "barrier": "gate",               # Gates
+        "entrance": "yes"                # Entrances
+    }
+
+    # park_boundries = gdf_park.geometry
+    # gdf_entries = ox.geometries_from_place(gdf_park, tags=tags)
+    # Request geometries with the specified tags
+    gdf_park = gdf_park.to_crs(epsg=4326)
+    park_boundary = gdf_park.unary_union
+    gdf_entries = ox.geometries_from_polygon(park_boundary, tags=tags)
+
+    print(f"gdf_entries: {type(gdf_entries)}")
+    print(f"gdf_park: {type(gdf_park)}")
+
+    # Ensure both GeoDataFrames have the same CRS (Coordinate Reference System)
+    # gdf_entries = gdf_entries.to_crs(gdf_park.crs)
+    gdf_entries = gdf_entries.to_crs(epsg=4326)
+
+    # Spatial join to get entry points within or intersecting the park boundary
+    gdf_entries_near_park = gpd.sjoin(gdf_entries, gdf_park, how="inner", predicate="intersects")
+
+    # Drop any duplicate columns from the join
+    gdf_entries_near_park = gdf_entries_near_park.loc[:, ~gdf_entries_near_park.columns.duplicated()]
+
+    # Get the boundary geometry (as a single merged geometry)
+    boundary = gdf_park.unary_union  # This creates a single geometry of the park's boundary
+
+    # Extract paths that intersect with the boundary
+    crossing_paths = gdf_entries[gdf_entries.intersects(boundary)]
+
+    print(f"crossing_paths: {type(crossing_paths)}")
+    print(f"gdf_entries_near_park: {type(gdf_entries_near_park)}")
+
+    # return crossing_paths
+    return gdf_entries_near_park
+
